19827599b78e8403c0756713897be4101a7f4eb5
emin aka <emin1234.aka@gmail.com>
2025-08-27 17:10:54 +0300

    guncellendi

--- aka/src/App.js
+++ aka/src/App.js
@@ -16,6 +16,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/skills" element={<Services />} />
           <Route path="/services" element={<Services />} />
           <Route path="/portfolio" element={<Portfolio />} />
           <Route path="/contact" element={<Contact />} />
